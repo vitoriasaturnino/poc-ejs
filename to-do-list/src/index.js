94bf265fa7da2da2b3618d5aee93794621cb7183
vitoriasaturnino <vim.saturnino@gmail.com>
2023-07-27 15:20:37 -0300

    feat: configure this project to use ejs templates

--- to-do-list/src/index.js
+++ to-do-list/src/index.js
@@ -1,10 +1,19 @@
 import express from 'express';
-const app = express();
+import * as dotenv from "dotenv";
+import path, { dirname } from 'path';
 
+dotenv.config();
+
+const app = express();
 const port = process.env.PORT || 3001;
 
+app.set("view engine", "ejs");
+app.set('views', path.join(dirname(''), 'src', 'views'));
+
+app.use(express.static(path.join(dirname(''), 'src', 'public')));
+
 app.get('/home', (req, res) => {
-  res.send('Hello World');
+  res.render('index');
 });
 
 app.listen(port, () =>
